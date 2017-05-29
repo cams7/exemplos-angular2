6cd68aa6f7d373cd343aa48375dd3a2248188a80
César A. Magalhães <ceanma@gmail.com>
2017-05-29 10:12:56 -0300

    Do canal do Youtube (Loiane Groner), foi realizada a aula (Curso Angular #48: Pipes: Async (assíncrono)). Acesse essa aula através do endereço (https://www.youtube.com/watch?v=WhcK7QOL6YA&index=49&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G)

---
+++ Loiane_Groner/pipes/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ExemplosPipeComponent } from './exemplos-pipe/exemplos-pipe.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe';
+import { SettingsService } from './settings.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipeComponent,
+    CamelCasePipe,
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: settingsService => settingsService.getLocale()
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Loiane_Groner/pipes/src/app/exemplos-pipe/exemplos-pipe.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+@Component({
+  selector: 'app-exemplos-pipe',
+  templateUrl: './exemplos-pipe.component.html',
+  styleUrls: ['./exemplos-pipe.component.css']
+})
+export class ExemplosPipeComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures and Algorithms 2nd ed',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glqjpRP'
+  };
+
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addCurso(valor){
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
+  obterCursos(){
+
+    if (this.livros.length === 0 || this.filtro === undefined
+    || this.filtro.trim() === ''){
+      return this.livros;
+    }
+
+    return this.livros.filter(
+       v => v.toLocaleLowerCase().includes(this.filtro.toLocaleLowerCase())
+    );
+  }
+  
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
+
+  valorAsync2 = Observable.interval(2000)
+    .map(valor => 'Valor assíncrono 2');
+
+
+}
